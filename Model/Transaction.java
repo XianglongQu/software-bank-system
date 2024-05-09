90dea743bec24b216d33dd7f6cf1d7528cf0a4d6
2thYunjinGu <152866806+2thYunjinGu@users.noreply.github.com>
2024-05-09 16:53:28 +0800

    Update Transaction.java

--- Model/Transaction.java
+++ Model/Transaction.java
@@ -1,21 +1,25 @@
 package Model;
-
 import java.time.LocalDateTime;
-
 public class Transaction {
-    private String type; // 交易类型（存款或取款）
-    private double amount; // 交易金额
-    private LocalDateTime timestamp; // 交易时间戳
-    private String duration; // 对于定期存款的持续时间
+    private String type;
+    private double amount;
+    private LocalDateTime timestamp;
+    private String duration;
+    private LocalDateTime lockUntil;  // 添加锁定到期时间
 
-    public Transaction(String type, double amount, String storageType, String duration) {
+    public Transaction(String type, double amount, String duration, LocalDateTime lockUntil) {
         this.type = type;
         this.amount = amount;
-        this.timestamp = LocalDateTime.now(); // 设置交易发生的当前时间
+        this.timestamp = LocalDateTime.now();
         this.duration = duration;
+        this.lockUntil = lockUntil;
     }
 
-    // 访问器方法
+    // Getter方法
+    public LocalDateTime getLockUntil() {
+        return lockUntil;
+    }
+
     public String getType() {
         return type;
     }
@@ -23,12 +27,7 @@ public class Transaction {
     public double getAmount() {
         return amount;
     }
-
     public LocalDateTime getTimestamp() {
         return timestamp;
-    }
-
-    public String getDuration() {
-        return duration;
     }
 }
