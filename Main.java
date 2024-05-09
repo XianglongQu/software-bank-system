62e732f1eabad03941a6de5f24bfd79634632e73
2thYunjinGu <152866806+2thYunjinGu@users.noreply.github.com>
2024-05-09 16:51:23 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,16 +2,27 @@ import javax.swing.SwingUtilities;
 import Model.User;
 import View.LoginView;
 import Control.LoginController;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
 
-            User user = User.loadUser("user.json");  // 从JSON文件加载用户
-            if (user == null) {
+            List<User> users = User.loadUsers("D:\\QM courses\\software engineering\\software engineering\\users.json");  // 从JSON文件加载用户
+            if (users.isEmpty()) {
                 System.out.println("系统故障，无法加载用户数据！");
+                return;
             }
+            User user1 = users.get(0);
             LoginView loginView = new LoginView();
-            new LoginController(loginView, user);
+            String username = loginView.getUsernameField().getText();
+            String password = new String(loginView.getPasswordField().getPassword());
+            for(int i=0; i< users.size();i++){
+                if( users.get(i).getUsername().equals(username) &&
+                        users.get(i).getUsername().equals(password) ){
+                    user1  = users.get(i);
+                }
+            }
+            new LoginController(loginView,user1);
         });
     }
 }
